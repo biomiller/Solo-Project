0c925c1b64067f642cc5380f064924f2dde351b3
biomiller <92millero@gmail.com>
2019-06-09 17:45:32 +0100

    removed import sql

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO ACCOUNT(id, accountNumber, firstName, lastName) VALUES (1, 1234, 'John', 'Doe');
-INSERT INTO ACCOUNT(id, accountNumber, firstName, lastName) VALUES (2, 1235, 'Jane', 'Doe');
-INSERT INTO ACCOUNT(id, accountNumber, firstName, lastName) VALUES (3, 1246, 'Jim', 'Taylor');
