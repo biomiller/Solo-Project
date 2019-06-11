2f591c1a3f428dc90c4f9119b1fd3eb3d2881d8f
biomiller <92millero@gmail.com>
2019-06-11 21:57:43 +0100

    fixed card entity getters and setters

--- src/main/java/com/bae/persistence/domain/Card.java
+++ src/main/java/com/bae/persistence/domain/Card.java
@@ -41,109 +41,212 @@ public class Card {
 	public Card() {
 		super();
 	}
-	
+
+
+
 	public int getCardId() {
 		return cardId;
 	}
+
+
+
 	public void setCardId(int cardId) {
 		this.cardId = cardId;
 	}
+
+
+
 	public String getName() {
 		return name;
 	}
+
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+
 	public String getMana_cost() {
 		return mana_cost;
 	}
+
+
+
 	public void setMana_cost(String mana_cost) {
 		this.mana_cost = mana_cost;
 	}
+
+
+
 	public int getCmc() {
 		return cmc;
 	}
+
+
+
 	public void setCmc(int cmc) {
 		this.cmc = cmc;
 	}
+
+
+
 	public String getType_line() {
 		return type_line;
 	}
+
+
+
 	public void setType_line(String type_line) {
 		this.type_line = type_line;
 	}
+
+
+
 	public String getOracle_text() {
 		return oracle_text;
 	}
+
+
+
 	public void setOracle_text(String oracle_text) {
 		this.oracle_text = oracle_text;
 	}
-	public int getLoyalty() {
+
+
+
+	public String getLoyalty() {
 		return loyalty;
 	}
-	public void setLoyalty(int loyalty) {
+
+
+
+	public void setLoyalty(String loyalty) {
 		this.loyalty = loyalty;
 	}
-	public int getPower() {
+
+
+
+	public String getPower() {
 		return power;
 	}
-	public void setPower(int power) {
+
+
+
+	public void setPower(String power) {
 		this.power = power;
 	}
-	public int getToughness() {
+
+
+
+	public String getToughness() {
 		return toughness;
 	}
-	public void setToughness(int toughness) {
+
+
+
+	public void setToughness(String toughness) {
 		this.toughness = toughness;
 	}
+
+
+
 	public char getColours_001() {
 		return colours_001;
 	}
+
+
+
 	public void setColours_001(char colours_001) {
 		this.colours_001 = colours_001;
 	}
+
+
+
 	public char getColours_002() {
 		return colours_002;
 	}
+
+
+
 	public void setColours_002(char colours_002) {
 		this.colours_002 = colours_002;
 	}
+
+
+
 	public char getColours_003() {
 		return colours_003;
 	}
+
+
+
 	public void setColours_003(char colours_003) {
 		this.colours_003 = colours_003;
 	}
+
+
+
 	public String getSet() {
 		return set;
 	}
+
+
+
 	public void setSet(String set) {
 		this.set = set;
 	}
+
+
+
 	public int getCollector_number() {
 		return collector_number;
 	}
+
+
+
 	public void setCollector_number(int collector_number) {
 		this.collector_number = collector_number;
 	}
+
+
+
 	public String getRarity() {
 		return rarity;
 	}
+
+
+
 	public void setRarity(String rarity) {
 		this.rarity = rarity;
 	}
-	public String getFlavour_text() {
+
+
+
+	public String getFlavor_text() {
 		return flavor_text;
 	}
-	public void setFlavour_text(String flavour_text) {
-		this.flavor_text = flavour_text;
+
+
+
+	public void setFlavor_text(String flavor_text) {
+		this.flavor_text = flavor_text;
 	}
+
+
+
 	public String getArtist() {
 		return artist;
 	}
+
+
+
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+	
+
 	
 
 }
