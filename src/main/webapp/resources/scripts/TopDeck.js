db7bc341aa41d6ebf8b9740ccfe594925b627d5c
biomiller <92millero@gmail.com>
2019-06-24 09:46:55 +0100

    fixed create deck

--- src/main/webapp/resources/scripts/TopDeck.js
+++ src/main/webapp/resources/scripts/TopDeck.js
@@ -167,6 +167,7 @@ function createNewDeck() {
         .then((resolve) => { location.href = 'user_decks.html' })
         .catch(function (error) { console.log(error.message) })
 
+    return false;
 }
 
 function createNewUser() {
